3c5f94a98529390a3b7d14888c2c6dd6a0280f2d
Michael Chukwu <imchukwu@gmail.com>
2022-08-03 15:15:47 +0100

    file upload slack bot

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3888700192052-3875496896215-lRUVpk8inLSNwayU8ei6skQd")
+	os.Setenv("CHANNEL_ID", "C03S4LL6STW")
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"Letterhead Paper.docx"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
